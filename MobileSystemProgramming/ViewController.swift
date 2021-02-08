cf4934e14fb4115e1b599870333adb18da23a632
staroi01 <illia.starodubtcev@kingfisher.com>
2021-02-08 17:51:46 +0200

    Initial Commit

---
+++ MobileSystemProgramming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileSystemProgramming
+//
+//  Created by Illia Starodubtcev on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
