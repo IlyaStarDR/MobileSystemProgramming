a56f0bcd3f7fc1ff15e1cd5174eae9b4d2d83450
staroi01 <illia.starodubtcev@kingfisher.com>
2021-02-28 15:00:50 +0200

    wip lab 2

--- MobileSystemProgramming/TabsViewControllers/PaintViewController/PaintViewController.swift
+++ MobileSystemProgramming/TabsViewControllers/PaintViewController/PaintViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class PaintViewController: UIViewController {
 
+    @IBOutlet weak var graphicView: GraphicView!
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            graphicView.isHidden = false
+            pieChartView.isHidden = true
+        case 1:
+            graphicView.isHidden = true
+            pieChartView.isHidden = false
+        default:
+            break
+        }
     }
 }
 
