7bac9f9c29f2dde84e6bb9777c713bd879a726b0
staroi01 <illia.starodubtcev@kingfisher.com>
2021-04-17 20:50:50 +0300

    WIP lab 4

---
+++ MobileSystemProgramming/Source/TabsViewControllers/BooksViewController/BookFullInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookFullInfoViewController.swift
+//  MobileSystemProgramming
+//
+//  Created by Illia Starodubtcev on 17.04.2021.
+//
+
+import UIKit
+
+class BookFullInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MobileSystemProgramming/Source/Views/BookInfoTableViewCell/BookInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookInfoTableViewCell.swift
+//  MobileSystemProgramming
+//
+//  Created by Illia Starodubtcev on 17.04.2021.
+//
+
+import UIKit
+
+class BookInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
